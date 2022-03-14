436ed116629123d87f53324ae6ccb849c957129c
DESKTOP-4MK2NG2\ht036 <3355359628@qq.com>
2022-03-14 13:48:06 +0800

    提交

--- src/main/java/com/xhtt/modules/accident/service/impl/AccidentReportServiceImpl.java
+++ src/main/java/com/xhtt/modules/accident/service/impl/AccidentReportServiceImpl.java
@@ -9,6 +9,7 @@ import com.xhtt.common.file.FileInfoModel;
 import com.xhtt.common.utils.*;
 import com.xhtt.modules.accident.controller.vo.AccidentReportSimpleVo;
 import com.xhtt.modules.event.service.EventReportService;
+import com.xhtt.modules.sys.entity.SysUserEntity;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
@@ -16,7 +17,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -89,24 +92,44 @@ public class AccidentReportServiceImpl extends ServiceImpl<AccidentReportDao, Ac
         accidentReportSimpleVo.setTownName(redis.get(accidentReportSimpleVo.getTownCode()));
     }
 
-//    @Override
-//    public Map<String, Object> getExportMap(Integer id, HttpServletResponse response) {
-//        Map<String, Object> map = new HashMap<>();
-//        AccidentReportEntity accidentReportEntity = getById(id);
-//        accidentReportEntity.
-//
-//                response.setCharacterEncoding("utf-8");
-//        response.setContentType("application/msword");
-//        // 设置浏览器以下载的方式处理该文件名
-//        String fileName = map.get("fileName") + ".docx";
-//        response.setHeader("Content-Disposition", "attachment;filename=".concat(String.valueOf(URLEncoder.encode(fileName, "UTF-8"))));
-////        WordUtils.exportWord(response, map, map.get("ftlFile").toString(), map.get("fileName").toString());
-//        try (OutputStream fos = response.getOutputStream()) {
-//            XWPFDocument doc = WordExportUtil.exportWord07("templates/risk/fxgzk" + map.get("level") + ".docx", map);
-//            doc.write(fos);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return map;
-//    }
+    @Override
+    public Map<String, Object> getExportMap(Integer id, HttpServletResponse response, SysUserEntity user) throws UnsupportedEncodingException {
+        Map<String, Object> map = new HashMap<>();
+        LocalDateTime now = LocalDateTime.now();
+        AccidentReportEntity accidentReportEntity = getById(id);
+
+        map.put("year", now.getYear());
+        map.put("moon", now.getMonth().toString());
+        map.put("day", now.toString());
+        map.put("companyName", accidentReportEntity.getCompanyName());
+        map.put("accidentTime", accidentReportEntity.getAccidentTime().toString());
+        map.put("accidentSite", accidentReportEntity.getAccidentSite());
+        map.put("accidentDescription", accidentReportEntity.getAccidentDescription());
+        map.put("receiveWay", accidentReportEntity.getReceiveWay());
+        map.put("receiveTime", accidentReportEntity.getReceiveTime().toString());
+        map.put("copyForUnit", accidentReportEntity.getCopyForUnit());
+        map.put("userName", user.getName());
+        map.put("signer", accidentReportEntity.getSigner());
+
+
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("application/msword");
+        // 设置浏览器以下载的方式处理该文件名
+        String fileName = map.get("fileName") + ".docx";
+
+        response.setHeader("Content-Disposition", "attachment;filename=".concat(String.valueOf(URLEncoder.encode(fileName, "UTF-8"))));
+//        WordUtils.exportWord(response, map, map.get("ftlFile").toString(), map.get("fileName").toString());
+        try (OutputStream fos = response.getOutputStream()) {
+            if (accidentReportEntity.getType() == 0){
+                XWPFDocument doc = WordExportUtil.exportWord07("templates/值班快报" + ".docx", map);
+                doc.write(fos);
+            }else {
+                XWPFDocument doc = WordExportUtil.exportWord07("templates/突发事件信息专报" + ".docx", map);
+                doc.write(fos);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
 }
